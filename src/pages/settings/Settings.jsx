f8c4df926f3da9382e6aa8131547d80ab19e3b07
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2025-06-02 22:07:45 +0100

    update settings

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -5,7 +5,7 @@ import UpdateUserDataForm from "../auth/UpdateUserDataForm";
 export default function Settings() {
 	return (
 		<Layout title={"Settings"}>
-			<div className="w-full flex flex-col gap-5 justify-center items-start overflow-y-auto min-h-screen">
+			<div className="w-full flex flex-col gap-5 justify-center items-start overflow-auto min-h-full">
 				<UpdateUserDataForm />
 				<UpdatePasswordForm />
 			</div>
